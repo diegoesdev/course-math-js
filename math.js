365180489b51ead368570b76d3ac758a753a6748
Diego Perales <diegoperalesdev@gmail.com>
2023-01-20 17:48:09 -0400

    Usar métodos de Math para simplificar el código

--- math.js
+++ math.js
@@ -46,3 +46,20 @@ calculosTriangulo(6, 6, 4, 5.5);
 
 console.groupEnd('Triangulo');
 
+// Circunferencia y área de un círculo
+
+console.group('Circulo');
+
+function calculosCirculo(radio) {
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio, 2);
+
+    return {
+        circunferencia: diametro * Math.PI.toFixed(4),
+        area: radioAlCuadrado * Math.PI.toFixed(3)
+    }
+}
+
+console.log(calculosCirculo(5));
+
+console.groupEnd('Circulo');
